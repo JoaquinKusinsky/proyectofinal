34eabc9bd31b1670849c8b328762e0c9b6d343de
JoaquinKusinsky <jkusinsky@udesa.edu.ar>
2023-06-16 18:16:58 -0300

    detallegenero y +

--- js/detallealbum.js
+++ js/detallealbum.js
@@ -2,3 +2,46 @@ function myFunction(){
     let element = document.body; //estoy guardando el nodo(body) en la variable element
     element.classList.toggle("light-mode") // lo que hace toggle es que busca en el css un clase que se relacione con el body y lo yo modifiqué en el css (light-mode)
 }
+
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('id');
+
+if (id) {
+  let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`;
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let albumDetails = document.querySelector(".album-details");
+
+      let albumListHTML = '';
+
+      for (let i = 0; i < data.data.length; i++) {
+        let album = data.data[i];
+
+        let songListHTML = generateSongList(album.tracks.data);
+
+        albumListHTML += `
+          <div>
+            <img class="album-cover" src="${album.cover_medium}" alt="Album Cover" />
+            <h2 class="album-title">${album.title}</h2>
+            <h3 class="artist-name">${album.artist.name}</h3>
+            <h4 class="genre-name">${album.genre.name}</h4>
+            <p class="release-date">Fecha de publicación: ${album.release_date}</p>
+            <h4>Canciones del disco:</h4>
+            <ul class="song-list">
+              ${songListHTML}
+            </ul>
+          </div>
+        `;
+      }
+
+      albumDetails.innerHTML = albumListHTML;
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+}
--- js/detalleartista.js
+++ js/detalleartista.js
@@ -2,3 +2,49 @@ function myFunction(){
     let element = document.body; //estoy guardando el nodo(body) en la variable element
     element.classList.toggle("light-mode") // lo que hace toggle es que busca en el css un clase que se relacione con el body y lo yo modifiqué en el css (light-mode)
 }
+
+
+// Obtener el ID del artista de la URL
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('id');
+
+if (id) {
+  let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`;
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let arts = document.querySelector(".arts");
+      let detalleArtista = `
+        <article class="listadoart">
+          <img class="imgart" src="${data.picture_medium}" alt="" />
+          <p class="part">${data.name}</p>
+        </article>
+      `;
+      arts.innerHTML = detalleArtista;
+
+      let albums = "<ul class='album-list'>";
+      if (data.albums && data.albums.data) {
+        let albumsData = data.albums.data;
+        for (let i = 0; i < albumsData.length && i < 5; i++) {
+          albums += `<li>${albumsData[i].title}</li>`;
+        }
+      }
+      albums += "</ul>";
+      arts.innerHTML += albums;
+
+      console.log(detalleArtista);
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
+} else {
+  console.log('No se proporcionó un ID de artista en la URL.');
+}
+
+
+
+
